3d1bbb97657ad3e375d5db95ffaeac61a7ec37cc
Ivan Santos <eng.ivancarlos85@gmail.com>
2022-10-31 13:22:33 -0300

    create book-moock class

---
+++ src/test/java/mock/BookMock.java
@@ -0,0 +1,31 @@
+package mock;
+
+import model.Book;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class BookMock {
+
+    public static boolean empty = false;
+
+    public static List<Book> personResponseList(){
+        if (empty) Stream.empty();
+        return Stream.of(bookMock(1L), bookMock(2L)).collect(Collectors.toList());
+    }
+
+    public static Book bookMock(Long id){
+        return Book.builder()
+                .id(id)
+                .title("Title")
+                .author("Author")
+                .yearOfPublication(2000)
+                .isbn("ISBN")
+                .genre("GENRE")
+                .statusBook("STATUS")
+                .dateRegister(new Date().toString())
+                .build();
+    }
+}
